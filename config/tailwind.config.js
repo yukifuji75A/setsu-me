62840f9d14853e75d8d1ad5b5863ede112a5f7c1
yukifuji75A <yuki.3.0957@icloud.com>
2026-07-04 18:57:17 +0900

    tailwind.config.jsを追加

---
+++ config/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/components/**/*.rb',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
