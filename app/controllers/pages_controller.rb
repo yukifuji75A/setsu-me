4835d00e072850562199e90167a32f99fc1dbdec
yukifuji75A <yuki.3.0957@icloud.com>
2026-07-06 17:02:13 +0900

    lint修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:top]
-  
+  skip_before_action :authenticate_user!, only: [ :top ]
+
   def top
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :profile, only: [:new, :create]
+  resource :profile, only: [ :new, :create ]
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "pages#top"
