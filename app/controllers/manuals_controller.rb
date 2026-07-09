daea6cfdfb2dfc987f6142a6ebe1d31557b26c49
yukifuji75A <yuki.3.0957@icloud.com>
2026-07-09 15:07:28 +0900

    プレビュー画面の実装

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -47,6 +47,8 @@ class ManualsController < ApplicationController
   end
 
   def step3
+    @profile = current_user.profile
+    @common_answers = common_answers_for(current_user)
     @basic_spec = session[:basic_spec]
     @handling_guide = session[:handling_guide]
   end
@@ -68,12 +70,28 @@ class ManualsController < ApplicationController
     session.delete(:basic_spec)
     session.delete(:handling_guide)
 
-    redirect_to mypage_path, notice: "トリセツを発行しました！"
+    redirect_to manual_path(manual), notice: "トリセツを発行しました！"
   rescue StandardError
     redirect_to step3_manuals_path, alert: "保存に失敗しました。もう一度お試しください。"
   end
 
+  def show
+    @manual = current_user.manuals.find(params[:id])
+    @profile = current_user.profile
+    @common_answers = common_answers_for(current_user)
+    @basic_spec = @manual.manual_ai_texts.find_by(section_type: :basic_spec)
+    @handling_guide = @manual.manual_ai_texts.find_by(section_type: :handling_guide)
+  end
+
   private
+
+  def common_answers_for(user)
+    user.answers
+        .joins(:question)
+        .where(questions: { theme: :common })
+        .includes(:question, :question_option)
+        .sort_by { |a| a.question.position }
+  end
 
   def answer_params
     params.fetch(:answers, {}).permit(
